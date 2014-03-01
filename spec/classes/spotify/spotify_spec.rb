618f1394d3c5ea9cdc8bfa99e65d9e9c08b108f4
Gareth Rushgrove <gareth@morethanseven.net>
2014-03-01 14:13:03 +0000

    update specs due to change in upstream apt module

--- spec/classes/spotify/spotify_spec.rb
+++ spec/classes/spotify/spotify_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe 'spotify', :type => :class do
+  let(:facts) {{
+    :osfamily => 'Debian',
+    :operatingsystem => 'Ubuntu',
+    :lsbdistid => 'debian',
+    :lsbdistcodename => 'precise'
+  }}
 
   context 'with no parameters' do
     it { should contain_package('spotify-client')}
