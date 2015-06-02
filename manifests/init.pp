ee67367307950a4eed8e3125db437b0dc3d4c606
Carlos Valiente <carlos@pepelabs.net>
2015-06-02 14:19:15 +0100

    Update to `puppetlabs-apt` 2.x

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,12 @@ class spotify {
     location          => 'http://repository.spotify.com',
     release           => 'stable',
     repos             => 'non-free',
-    key               => '94558F59',
-    include_src       => false,
+    key               => {
+      id              => '94558F59',
+    },
+    include           => {
+      src             => false,
+    },
   }
 
   package { 'spotify-client':
